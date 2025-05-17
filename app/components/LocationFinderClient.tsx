d38c7f02c755c9c112bef24ada77e5ecc54eef32
Dawda Njai <dknjai1@madisoncollege.edu>
2025-05-17 00:26:27 -0500

    Update app with new changes

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -2,27 +2,24 @@
 
 import { useEffect, useState } from "react";
 
-export default function LocationFinderClient() {
-  const [locationInfo, setLocationInfo] = useState<{ city?: string }>({});
+export default function LocationFinderServer() {
+  const [locationInfo, setLocationInfo] = useState({city: 'N/A'});
 
   useEffect(() => {
     const getLocationInfo = async () => {
-      try {
+  
         const response = await fetch("https://ipwho.is/");
         const locationData = await response.json();
         console.log("Client location:", locationData);
-        setLocationInfo(locationData);
-      } catch (err) {
-        console.error("Failed to get location info", err);
-      }
-    };
+        setLocationInfo(locationData);  
+    }
 
     getLocationInfo();
-  }, []); // ✅ prevents infinite re-renders
+  })
 
   return (
     <>
-      <h1>Hello from {locationInfo?.city} – client com</h1>
+      <h1>Hello from {locationInfo.city}  client com</h1>
     </>
   );
 }
