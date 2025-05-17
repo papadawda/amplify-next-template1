3e3296f2ed6160183228fc876e472a59d804dcdb
Dawda Njai <dknjai1@madisoncollege.edu>
2025-05-17 01:00:12 -0500

    Update app with new changes

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -6,11 +6,10 @@ export default function LocationFinderServer() {
   const [locationInfo, setLocationInfo] = useState({city: 'N/A'});
 
   const getLocationInfo = async () => {
-      const response = await fetch("https://ipwho.is/");
-      const locationData = await response.json();
-      console.log(locationData); 
-      setLocationInfo(locationData);
-   
+    const response = await fetch("https://ipapi.co/json/");
+    const locationData = await response.json();
+    console.log(locationData); 
+    setLocationInfo(locationData);
   }
 
   useEffect(() => {
--- app/components/LocationFinderServer.tsx
+++ app/components/LocationFinderServer.tsx
@@ -1,9 +1,9 @@
 export default async function LocationFinderServer() {
 
-      const response = await fetch("https://ipwho.is/");
-      const locationData = await response.json();
-      console.log(locationData); 
-      const locationInfo = locationData;
+    const response = await fetch("https://ipwho.is/");
+    const locationData = await response.json();
+    console.log(locationData); 
+    const locationInfo = locationData;
 
   return (
     <>
