0a9512075e1625fa907573bcf77681aee58f10db
Dawda Njai <dknjai1@madisoncollege.edu>
2025-12-11 20:21:20 -0600

    Trying out servers
    Trying out server vs client components

---
+++ app/components/LocationFinderServer.tsx
@@ -0,0 +1,15 @@
+
+export default async function LocationFinderServer() {
+  
+        const response = await fetch('https://apip.cc/json');
+        const locationData = await response.json();
+        console.log(locationData);
+        const locationInfo = locationData;
+   
+
+    return (
+        <>
+            <h1>Hello from {locationInfo?.City} - server component</h1>
+        </>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
-import AuthenticatorWrapper from "./AuthenticatorWrapper";
+
 import "@aws-amplify/ui-react/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,9 +21,7 @@ export default function RootLayout({
        
     <html lang="en">
       <body>      
-        <AuthenticatorWrapper>
           {children}
-        </AuthenticatorWrapper>
       </body>
     </html>
   );
