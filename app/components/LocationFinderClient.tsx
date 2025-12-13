73673714471210609a7461aafbbd488fde19040c
Dawda Njai <dknjai1@madisoncollege.edu>
2025-12-13 11:53:57 -0600

    added weather info

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -3,12 +3,25 @@ import { useEffect, useState } from "react"
 
 export default function LocationFinderClient() {
     const [locationInfo, setLocationInfo] = useState({City: 'N/A'})
+    const [tempC, setTempC] = useState('?');
 
     const getLocationInfo = async () => {
         const response = await fetch('https://apip.cc/json');
         const locationData = await response.json();
         console.log(locationData, "testing");
-        setLocationInfo(locationData)
+        setLocationInfo(locationData);
+
+        const url =
+      "https://www.7timer.info/bin/astro.php?lon=" +
+      locationData.Longitude +
+      "&lat=" +
+      locationData.Latitude +
+      "&ac=0&unit=metric&output=json&tzshift=0";
+
+    const weatherRes = await fetch(url);
+    const weatherData = await weatherRes.json();
+
+    setTempC(weatherData.dataseries[0].temp2m);
     }
 
 
@@ -20,6 +33,7 @@ export default function LocationFinderClient() {
     return (
         <>
             <h1>Hello from {locationInfo.City} - client component</h1>
+            <p>Current temperature: {tempC}°C</p>
         </>
     )
 }
